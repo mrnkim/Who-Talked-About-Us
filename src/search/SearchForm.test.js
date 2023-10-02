a59b9294e5ff7946d9abe22e50f69a2eeb73c00a
MKim <mrn.kim07@gmail.com>
2023-10-02 18:36:59 +0900

    Clean up tests

--- src/search/SearchForm.test.js
+++ src/search/SearchForm.test.js
@@ -26,32 +26,3 @@ it("updates the form input on change", function () {
 
   expect(input.value).toBe("test query");
 });
-
-// it("calls the search function with trimmed query on form submission with valid input", function () {
-//   const mockSearchFunction = jest.fn();
-//   const { getByTestId, getByText } = render(
-//     <SearchForm index="test-index" search={mockSearchFunction} />
-//   );
-
-//   // Simulate typing "test query" in the input field
-//   const input = getByTestId("search-input");
-//   fireEvent.change(input, { target: { value: "test query" } });
-
-//   // Click the submit button
-//   const submitButton = getByText("Search");
-//   fireEvent.click(submitButton);
-
-//   // Check if the search function is called with the correct arguments
-//   expect(mockSearchFunction).toHaveBeenCalledWith("test-index", "test query");
-// });
-
-// it("displays an error message when the form is submitted with empty input", function () {
-//   const { getByTestId, getByText } = render(<SearchForm />);
-//   const input = getByTestId("search-input");
-//   const submitButton = getByText("Search");
-
-//   fireEvent.click(submitButton);
-
-//   const errorMessage = getByText("Please enter the search term");
-//   expect(errorMessage).toBeInTheDocument();
-// });
