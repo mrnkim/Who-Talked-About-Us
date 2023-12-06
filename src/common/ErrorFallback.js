5795d5f830cd39418630e69b8f196f02ced7917c
MKim <mrn.kim07@gmail.com>
2023-12-06 11:13:24 -0800

    Update styling of ErrorFallback

--- src/common/ErrorFallback.js
+++ src/common/ErrorFallback.js
@@ -1,10 +1,15 @@
 import React from "react";
+import WarningIcon from "../svg/Warning.svg";
+import "./ErrorFallback.css";
 
 function ErrorFallback({ error, resetErrorBoundary, setIndexId }) {
   return (
     <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
+      <p>Something went wrong</p>
+      <div className="warningMessageWrapper">
+        <img src={WarningIcon} alt="WarningIcon" className="icon"></img>
+        <div className="warningMessage">{error.message}</div>
+      </div>
       <button className="resetButton" onClick={() => setIndexId(null)}>
         Go back
       </button>
